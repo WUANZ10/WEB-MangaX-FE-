858d5259678e3cd7d72ce737f23d638188dfd544
SandieYT <sandie594759@gmail.com>
2025-02-11 19:13:10 +0700

    changed the gui a bit
    changed the gui a bit

--- src/components/SliderComponent/SliderComponent.jsx
+++ src/components/SliderComponent/SliderComponent.jsx
@@ -9,7 +9,7 @@ export default function SliderComponent({ arrImages }) {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 5000,
   };
   return (
     <div>
