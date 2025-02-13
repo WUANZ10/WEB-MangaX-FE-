3269bdf05d16e2cb728b9addf1db3b27bdda6653
WUANZ10 <nguyenquangmexio100@gmail.com>
2025-02-13 17:27:53 +0700

    fix login && register

--- src/components/LoginComponent/LoginComponent.jsx
+++ src/components/LoginComponent/LoginComponent.jsx
@@ -1,63 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./loginStyle.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export default function LoginComponent() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login successful!");
+  };
+
   return (
     <div className="login_content">
-      <div className="input_screen">
-        <ul className="form">
-          <li>
-            <p
-              className="label text_email_or_username"
-              placeholder="Tên đăng nhập hoặc email"
-            >
-              Tên đăng nhập hoặc email
-            </p>
-            <input id="login_username" />
-          </li>
+      <form onSubmit={handleSubmit}>
+        <div className="input_screen">
+          <ul className="form">
+            <li>
+              <p className="label text_email_or_username">
+                Tên đăng nhập hoặc email
+              </p>
+              <input id="login_username" required="required" />
+            </li>
 
-          <li>
-            <p className="label text_password" placeholder="Mật khẩu">
-              Mật khẩu
-            </p>
-            <input className="password" type="password" id="login_password" />
-          </li>
-        </ul>
-      </div>
-      <p className="forgot">
-        <Link to="#">Quên mật khẩu?</Link>
-      </p>
-      <div className="button_click">
-        <button
-          className="button_yes text_button_login"
-          placeholder="Đăng nhập"
-        >
-          Đăng nhập
-        </button>
-      </div>
-      <div className="other_login">
-        <div className="title">
-          <p className="text_or" placeholder="Hoặc">
-            Hoặc
-          </p>
+            <li>
+              <p className="label text_password">Mật khẩu</p>
+              <div className="div_password">
+                <input
+                  className="password"
+                  type={showPassword ? "text" : "password"}
+                  id="login_password"
+                  required="required"
+                />
+                <span onClick={togglePasswordVisibility}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </span>
+              </div>
+            </li>
+          </ul>
         </div>
-        <div className="list">
-          <div className="item">
-            <img
-              className="item_icon"
-              src="https://cmangax.com/assets/img/icon/login_google.png"
-              alt=""
-            />
-            <text
-              className="text_login_with_google"
-              placeholder="Đăng nhập với Google"
-            >
-              Đăng nhập với Google
-            </text>
+        <p className="forgot">
+          <Link to="#">Quên mật khẩu?</Link>
+        </p>
+        <div className="button_click">
+          <button className="button_yes text_button_login" type="submit">
+            Đăng nhập
+          </button>
+        </div>
+        <div className="other_login">
+          <div className="title">
+            <p className="text_or">Hoặc</p>
+          </div>
+          <div className="list">
+            <div className="item">
+              <img
+                className="item_icon"
+                src="https://cmangax.com/assets/img/icon/login_google.png"
+                alt=""
+              />
+              <text className="text_login_with_google">
+                Đăng nhập với Google
+              </text>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/RegisterComponent/RegisterComponent.jsx
+++ src/components/RegisterComponent/RegisterComponent.jsx
@@ -1,57 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export default function RegisterComponent() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [isPasswordMatch, setIsPasswordMatch] = useState(true);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    checkPasswordMatch(e.target.value, rePassword);
+  };
+
+  const handleRePasswordChange = (e) => {
+    setRePassword(e.target.value);
+    checkPasswordMatch(password, e.target.value);
+  };
+
+  const checkPasswordMatch = (password, rePassword) => {
+    setIsPasswordMatch(password === rePassword);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isPasswordMatch) {
+      console.log("Registration successful!");
+    } else {
+      console.log("Passwords do not match!");
+    }
+  };
+
   return (
     <div className="register_content">
-      <div className="input_screen">
-        <ul className="form">
-          <li>
-            <p className="label text_username" placeholder="Tên đăng nhập">
-              Tên đăng nhập
-            </p>
-            <input id="reg_username" placeholder />
-          </li>
-          <li>
-            <p className="label text_password" placeholder="Mật khẩu">
-              Mật khẩu
-            </p>
-            <input
-              className="password"
-              type="password"
-              id="reg_password"
-              placeholder
-            />
-          </li>
-          <li>
-            <p
-              className="label text_repassword"
-              placeholder="Nhập lại mật khẩu"
-            >
-              Nhập lại mật khẩu
-            </p>
-            <input
-              className="password"
-              type="password"
-              id="reg_re_password"
-              placeholder
-            />
-          </li>
-          <li>
-            <p className="label text_profile_name" placeholder="Tên hiển thị">
-              Tên hiển thị
-            </p>
-            <input id="reg_name" placeholder />
-          </li>
-        </ul>
-      </div>
-      <div className="button_click">
-        <button
-          className="button_yes full text_button_register"
-          placeholder="Đăng ký"
-        >
-          Đăng ký
-        </button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="input_screen">
+          <ul className="form">
+            <li>
+              <p className="label text_username">Tên đăng nhập</p>
+              <input id="reg_username" placeholder required="required" />
+            </li>
+            <li>
+              <p className="label text_password">Mật khẩu</p>
+              <div className="div_password">
+                <input
+                  className="password"
+                  type={showPassword ? "text" : "password"}
+                  id="reg_password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required="required"
+                />
+                <span onClick={togglePasswordVisibility}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </span>
+              </div>
+            </li>
+            <li>
+              <p className="label text_repassword">Nhập lại mật khẩu</p>
+              <div className="div_password">
+                <input
+                  className={`password ${
+                    !isPasswordMatch && rePassword ? "input-error" : ""
+                  }`}
+                  type={showPassword ? "text" : "password"}
+                  id="reg_re_password"
+                  value={rePassword}
+                  onChange={handleRePasswordChange}
+                  required="required"
+                />
+                <span onClick={togglePasswordVisibility}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </span>
+              </div>
+            </li>
+            <li>
+              <p className="label text_profile_name">Tên hiển thị</p>
+              <input id="reg_name" required="required" />
+            </li>
+          </ul>
+        </div>
+        <div className="button_click">
+          <button
+            className="button_yes full text_button_register"
+            type="submit"
+            disabled={!isPasswordMatch}
+          >
+            Đăng ký
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
