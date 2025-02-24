942586fb9cd6f8565f30f6433be1e0fd5173db2c
WUANZ10 <nguyenquangmexio100@gmail.com>
2025-02-24 22:29:27 +0700

    1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import HomePage from "../pages/HomePage/HomePage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import Profile from "../pages/Profile/Profile";
-import Register from "../pages/RegisterPage/Register";
 
 export const routes = [
   {
