0c2a83f7160cd685d65d07bd09d0c187063199b9
SandieYT <sandie594759@gmail.com>
2025-03-14 15:01:21 +0700

    some changes

--- src/pages/HomePage/Album/Album.jsx
+++ src/pages/HomePage/Album/Album.jsx
@@ -13,23 +13,23 @@ const ComicButton = ({ album }) => {
 
   return <div className="album-item-wrapper">
     <button onClick={handleClick} key={album._id} className="album-item">
-  <img className="album-logo" src={album.cover_image}/>
+  <img className="album-logo" src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg"/>
   <h3 className="album-name">{album.title}</h3>
   <div className="stats-row">
   <div className="stats-column">
-              <IoEyeSharp />
+              <IoEyeSharp className="icon" />
               <p className="p value">{album.views}</p>
             </div>
             <div className="stats-column">
-              <IoBookmarkSharp />
+              <IoBookmarkSharp className="icon" />
               <p className="p value">{album.favorites}</p>
             </div>
             <div className="stats-column">
-              <IoChatbubble />
+              <IoChatbubble className="icon" />
               <p className="p value">0</p>
             </div>
             <div className="stats-column">
-              <FaBookOpenReader />
+              <FaBookOpenReader className="icon" />
               <p className="p value">{album.chapters.length}</p>
             </div>
   </div>
