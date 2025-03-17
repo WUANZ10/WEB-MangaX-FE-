edba3ddd049a17af06f2793d452050d64a8ad031
WUANZ10 <nguyenquangmexio100@gmail.com>
2025-03-17 11:04:20 +0700

    12

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './slides/counterSlice'
-=======
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slides/counterSlice";
 import userReducer from "./slides/userSlice"
->>>>>>> master
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-<<<<<<< HEAD
-  },
-})
-=======
     user: userReducer,
   },
-});
->>>>>>> master
+})
--- src/routes/Private.jsx
+++ src/routes/Private.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-
-export default function Private() {
-  const isAuthenticated = true;
-
-  return isAuthenticated ? <Outlet /> : <Navigate to="/home" replace />;
-=======
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -15,5 +6,4 @@ export default function Private() {
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
 
   return isLoggedIn ? <Outlet /> : <Navigate to="/home" replace />;
->>>>>>> master
 }
